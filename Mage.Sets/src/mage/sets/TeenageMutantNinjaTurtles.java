550796bb239eecb98cd34a5540df5129dafbee66
theelk801 <theelk801@gmail.com>
2026-02-23 16:56:10 -0500

    [TMT] Implement Fugitive Droid

--- Mage.Sets/src/mage/sets/TeenageMutantNinjaTurtles.java
+++ Mage.Sets/src/mage/sets/TeenageMutantNinjaTurtles.java
@@ -97,6 +97,7 @@ public final class TeenageMutantNinjaTurtles extends ExpansionSet {
         cards.add(new SetCardInfo("Forest", 314, Rarity.LAND, mage.cards.basiclands.Forest.class, FULL_ART_BFZ_VARIOUS));
         cards.add(new SetCardInfo("Forest", 319, Rarity.LAND, mage.cards.basiclands.Forest.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Frog Butler", 114, Rarity.COMMON, mage.cards.f.FrogButler.class));
+        cards.add(new SetCardInfo("Fugitive Droid", 40, Rarity.UNCOMMON, mage.cards.f.FugitiveDroid.class));
         cards.add(new SetCardInfo("General Traag, Heart of Stone", 90, Rarity.UNCOMMON, mage.cards.g.GeneralTraagHeartOfStone.class));
         cards.add(new SetCardInfo("Genghis Frog", 148, Rarity.UNCOMMON, mage.cards.g.GenghisFrog.class));
         cards.add(new SetCardInfo("Go Ninja Go", 149, Rarity.UNCOMMON, mage.cards.g.GoNinjaGo.class));
