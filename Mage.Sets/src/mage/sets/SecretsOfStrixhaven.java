b948ed36b4c6d431394a319b6fd1be1fa78b244f
theelk801 <theelk801@gmail.com>
2026-04-02 13:21:54 -0400

    [SOS] Implement Muse Seeker

--- Mage.Sets/src/mage/sets/SecretsOfStrixhaven.java
+++ Mage.Sets/src/mage/sets/SecretsOfStrixhaven.java
@@ -79,6 +79,7 @@ public final class SecretsOfStrixhaven extends ExpansionSet {
         cards.add(new SetCardInfo("Mountain", 270, Rarity.LAND, mage.cards.basiclands.Mountain.class, FULL_ART_BFZ_VARIOUS));
         cards.add(new SetCardInfo("Mountain", 278, Rarity.LAND, mage.cards.basiclands.Mountain.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Mountain", 279, Rarity.LAND, mage.cards.basiclands.Mountain.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Muse Seeker", 60, Rarity.UNCOMMON, mage.cards.m.MuseSeeker.class));
         cards.add(new SetCardInfo("Old-Growth Educator", 207, Rarity.UNCOMMON, mage.cards.o.OldGrowthEducator.class));
         cards.add(new SetCardInfo("Oracle's Restoration", 156, Rarity.COMMON, mage.cards.o.OraclesRestoration.class));
         cards.add(new SetCardInfo("Paradox Gardens", 258, Rarity.COMMON, mage.cards.p.ParadoxGardens.class));
