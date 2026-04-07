eb03e171ab67a0c08ddcb23bf81debe26c4b1503
theelk801 <theelk801@gmail.com>
2026-04-07 11:01:58 -0400

    [SOS] Implement Mind into Matter

--- Mage.Sets/src/mage/sets/SecretsOfStrixhaven.java
+++ Mage.Sets/src/mage/sets/SecretsOfStrixhaven.java
@@ -97,6 +97,7 @@ public final class SecretsOfStrixhaven extends ExpansionSet {
         cards.add(new SetCardInfo("Maelstrom Artisan", 334, Rarity.RARE, mage.cards.m.MaelstromArtisan.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Mathemagics", 320, Rarity.MYTHIC, mage.cards.m.Mathemagics.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Mathemagics", 58, Rarity.MYTHIC, mage.cards.m.Mathemagics.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Mind into Matter", 202, Rarity.RARE, mage.cards.m.MindIntoMatter.class));
         cards.add(new SetCardInfo("Molten Note", 204, Rarity.UNCOMMON, mage.cards.m.MoltenNote.class));
         cards.add(new SetCardInfo("Molten-Core Maestro", 125, Rarity.RARE, mage.cards.m.MoltenCoreMaestro.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Molten-Core Maestro", 335, Rarity.RARE, mage.cards.m.MoltenCoreMaestro.class, NON_FULL_USE_VARIOUS));
