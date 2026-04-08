536acaeda5ab0984f7c0943a3f120c503cb82314
theelk801 <theelk801@gmail.com>
2026-04-08 10:07:14 -0400

    [SOS] Implement Mica, Reader of Ruins

--- Mage.Sets/src/mage/sets/SecretsOfStrixhaven.java
+++ Mage.Sets/src/mage/sets/SecretsOfStrixhaven.java
@@ -98,6 +98,7 @@ public final class SecretsOfStrixhaven extends ExpansionSet {
         cards.add(new SetCardInfo("Maelstrom Artisan", 334, Rarity.RARE, mage.cards.m.MaelstromArtisan.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Mathemagics", 320, Rarity.MYTHIC, mage.cards.m.Mathemagics.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Mathemagics", 58, Rarity.MYTHIC, mage.cards.m.Mathemagics.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Mica, Reader of Ruins", 124, Rarity.UNCOMMON, mage.cards.m.MicaReaderOfRuins.class));
         cards.add(new SetCardInfo("Mind into Matter", 202, Rarity.RARE, mage.cards.m.MindIntoMatter.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Mind into Matter", 352, Rarity.RARE, mage.cards.m.MindIntoMatter.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Molten Note", 204, Rarity.UNCOMMON, mage.cards.m.MoltenNote.class));
