c924fa01913c8b9f9b1d63d2f43466868a0fc9ff
theelk801 <theelk801@gmail.com>
2026-04-09 14:52:05 -0400

    [SOS] Implement Kirol, History Buff

--- Mage.Sets/src/mage/sets/SecretsOfStrixhaven.java
+++ Mage.Sets/src/mage/sets/SecretsOfStrixhaven.java
@@ -101,6 +101,7 @@ public final class SecretsOfStrixhaven extends ExpansionSet {
         cards.add(new SetCardInfo("Jadzi, Steward of Fate", 55, Rarity.RARE, mage.cards.j.JadziStewardOfFate.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Joined Researchers", 23, Rarity.RARE, mage.cards.j.JoinedResearchers.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Joined Researchers", 312, Rarity.RARE, mage.cards.j.JoinedResearchers.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Kirol, History Buff", 198, Rarity.UNCOMMON, mage.cards.k.KirolHistoryBuff.class));
         cards.add(new SetCardInfo("Landscape Painter", 56, Rarity.COMMON, mage.cards.l.LandscapePainter.class));
         cards.add(new SetCardInfo("Lluwen, Exchange Student", 199, Rarity.UNCOMMON, mage.cards.l.LluwenExchangeStudent.class));
         cards.add(new SetCardInfo("Lorehold Charm", 200, Rarity.UNCOMMON, mage.cards.l.LoreholdCharm.class, NON_FULL_USE_VARIOUS));
