8eb85d729b6c0b0c3acf08aa376d17def6f7e29d
Grath <1895280+Grath@users.noreply.github.com>
2026-04-09 12:48:55 -0400

    [SOS] Implement Grave Researcher

--- Mage.Sets/src/mage/sets/SecretsOfStrixhaven.java
+++ Mage.Sets/src/mage/sets/SecretsOfStrixhaven.java
@@ -79,6 +79,8 @@ public final class SecretsOfStrixhaven extends ExpansionSet {
         cards.add(new SetCardInfo("Garrison Excavator", 116, Rarity.UNCOMMON, mage.cards.g.GarrisonExcavator.class));
         cards.add(new SetCardInfo("Graduation Day", 16, Rarity.UNCOMMON, mage.cards.g.GraduationDay.class));
         cards.add(new SetCardInfo("Grapple with Death", 192, Rarity.COMMON, mage.cards.g.GrappleWithDeath.class));
+        cards.add(new SetCardInfo("Grave Researcher", 325, Rarity.RARE, mage.cards.g.GraveResearcher.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Grave Researcher", 85, Rarity.RARE, mage.cards.g.GraveResearcher.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Group Project", 17, Rarity.UNCOMMON, mage.cards.g.GroupProject.class));
         cards.add(new SetCardInfo("Hardened Academic", 194, Rarity.RARE, mage.cards.h.HardenedAcademic.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Hardened Academic", 351, Rarity.RARE, mage.cards.h.HardenedAcademic.class, NON_FULL_USE_VARIOUS));
