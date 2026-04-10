f45bebca969575041cdf6600836c31067cd0bf09
theelk801 <theelk801@gmail.com>
2026-04-10 15:08:47 -0400

    [SOS] Implement Strife Scholar

--- Mage/src/main/java/mage/cards/PrepareCard.java
+++ Mage/src/main/java/mage/cards/PrepareCard.java
@@ -12,6 +12,10 @@ public abstract class PrepareCard extends CardImpl {
 
     protected Card spellCard;
 
+    protected PrepareCard(UUID ownerId, CardSetInfo setInfo, CardType[] types, String costs, String preparationName, CardType typeSpell, String costsSpell) {
+        this(ownerId, setInfo, types, costs, preparationName, new CardType[]{typeSpell}, costsSpell);
+    }
+
     protected PrepareCard(UUID ownerId, CardSetInfo setInfo, CardType[] types, String costs, String preparationName, CardType[] typesSpell, String costsSpell) {
         super(ownerId, setInfo, types, costs);
         this.spellCard = new PrepareSpellCard(ownerId, setInfo, preparationName, typesSpell, costsSpell, this);
