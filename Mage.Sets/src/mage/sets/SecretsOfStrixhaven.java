984aac9d3f0b494bcce5b07070096238cd10ecf8
theelk801 <theelk801@gmail.com>
2026-04-10 15:14:53 -0400

    [SOS] Implement Melancholic Poet

--- Mage.Sets/src/mage/sets/SecretsOfStrixhaven.java
+++ Mage.Sets/src/mage/sets/SecretsOfStrixhaven.java
@@ -129,6 +129,7 @@ public final class SecretsOfStrixhaven extends ExpansionSet {
         cards.add(new SetCardInfo("Mage Tower Referee", 249, Rarity.COMMON, mage.cards.m.MageTowerReferee.class));
         cards.add(new SetCardInfo("Mathemagics", 320, Rarity.MYTHIC, mage.cards.m.Mathemagics.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Mathemagics", 58, Rarity.MYTHIC, mage.cards.m.Mathemagics.class, NON_FULL_USE_VARIOUS));
+        cards.add(new SetCardInfo("Melancholic Poet", 90, Rarity.COMMON, mage.cards.m.MelancholicPoet.class));
         cards.add(new SetCardInfo("Mica, Reader of Ruins", 124, Rarity.UNCOMMON, mage.cards.m.MicaReaderOfRuins.class));
         cards.add(new SetCardInfo("Mind into Matter", 202, Rarity.RARE, mage.cards.m.MindIntoMatter.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Mind into Matter", 352, Rarity.RARE, mage.cards.m.MindIntoMatter.class, NON_FULL_USE_VARIOUS));
